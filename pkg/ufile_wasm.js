788728b813d240500c75f816070bee1ee52510ce
rsomonte <rsomonte@users.noreply.github.com>
2025-07-30 01:34:04 +0000

    deploy: 22a4b222165e12f8f88b7964836d323be548331b

--- pkg/ufile_wasm.js
+++ pkg/ufile_wasm.js
@@ -145,7 +145,7 @@ async function __wbg_init(module_or_path) {
     }
 
     if (typeof module_or_path === 'undefined') {
-        module_or_path = new URL('filetype_wasm_bg.wasm', import.meta.url);
+        module_or_path = new URL('ufile_wasm_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
